e05226e9c456cdedbe1b0ebe9217f87343ee41a5
Kira Dinan <kiradinan.dev@gmail.com>
2026-04-03 13:48:49 -0300

    Fix for collections

--- Editor/Dividers/DividerAttributeDrawer.cs
+++ Editor/Dividers/DividerAttributeDrawer.cs
@@ -1,33 +1,50 @@
 using UnityEditor;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Fsi.Ui.Dividers
 {
-	[CustomPropertyDrawer(typeof(DividerAttribute))]
-	public class DividerAttributeDrawer : DecoratorDrawer
-	{
-		public override VisualElement CreatePropertyGUI()
-		{
-			DividerAttribute attr = (DividerAttribute)attribute;
-			VisualElement spacer = new()
-			                       {
-				                       style =
-				                       {
-					                       width = new StyleLength(StyleKeyword.Auto),
-					                       height = 5,
-					                       
-					                       flexGrow = 0,
-					                       flexShrink = 0,
-					                       
-					                       backgroundColor = attr.Color,
-					                       
-					                       marginTop = 7.5f,
-					                       marginRight = 5,
-					                       marginBottom = 7.5f,
-					                       marginLeft = 5,
-				                       }
-			                       };
-			return spacer; // new Divider();
-		}
-	}
+    [CustomPropertyDrawer(typeof(DividerAttribute))]
+    public class DividerAttributeDrawer : DecoratorDrawer
+    {
+        private DividerAttribute Attr => (DividerAttribute)attribute;
+
+        public override VisualElement CreatePropertyGUI()
+        {
+            var container = new VisualElement();
+            container.style.marginTop = 7.5f;
+            container.style.marginRight = 5f;
+            container.style.marginBottom = 7.5f;
+            container.style.marginLeft = 5f;
+            container.pickingMode = PickingMode.Ignore;
+
+            var line = new VisualElement();
+            line.style.height = Attr.Size;
+            line.style.backgroundColor = Attr.Color;
+            line.style.flexGrow = 1;
+            line.pickingMode = PickingMode.Ignore;
+
+            container.Add(line);
+            return container;
+        }
+
+        public override float GetHeight()
+        {
+            return Attr.Size + 15f; // line + vertical margins
+        }
+
+        public override void OnGUI(Rect position)
+        {
+            float y = position.y + 7.5f;
+            float height = Attr.Size;
+
+            var lineRect = new Rect(
+                                    position.x + 5f,
+                                    y,
+                                    position.width - 10f,
+                                    height);
+
+            EditorGUI.DrawRect(lineRect, Attr.Color);
+        }
+    }
 }
--- Runtime/Dividers/DividerAttribute.cs
+++ Runtime/Dividers/DividerAttribute.cs
@@ -3,21 +3,17 @@ using UnityEngine;
 
 namespace Fsi.Ui.Dividers
 {
-	[AttributeUsage(AttributeTargets.Field
-	                | AttributeTargets.Property
-	                | AttributeTargets.Class
-	                | AttributeTargets.Method,
-	                AllowMultiple = true)]
+	[AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
 	public class DividerAttribute : PropertyAttribute
 	{
 		private const float Shade = 0.19f;
 		private const float DefaultSize = 4f;
-		private Color DefaultColor => Color.red; // new(Shade, Shade, Shade, 1f);
+		private Color DefaultColor => new(Shade, Shade, Shade, 1f);
 		
 		public float Size { get; }
 		public Color Color { get; }
 		
-		public DividerAttribute()
+		public DividerAttribute() : base(true)
 		{
 			Size = DefaultSize;
 			Color = DefaultColor;
